575b15f61e5f34529f2fe8c6ac6be7d48889dac1
LJKuhn <1221kuhn@gmail.com>
2024-04-07 23:27:47 -0300

    PEmisor2.ino and PReceptor2.ino are finished and tested

--- PEmisor2/PEmisor2.ino
+++ PEmisor2/PEmisor2.ino
@@ -1,89 +1,129 @@
-#define BotonEncenderApagar 2  // Pin del pulsador encender/apagar
-#define BotonCambiarColor 3    // Pin del pulsador cambiar
-
-#define LEDREC 4               //Pin del LED1 (acuse de recibido)
+#define LEDRECP 4               //Pin del LED1 (acuse de recibido)
 #define LEDCON 5               // Pin del LED2 (conexión establecida)
 
-#define tiempoC 2000
-#define tiempoR 2000
+unsigned long antirebote1 = 0;  //variables para hacer el anti rebote de los pulsadores
+unsigned long  antirebote2 = 0;
+int tiempoR = 2000;
 
-volatile bool power = true;
+volatile bool power = true;  //variables que utilizaremos para controlar los estados de prendiso/apagado y cambio de color
 volatile int cont = 1;
-int state;
 
-int R = 0;
+int state;                   //variable donde almacenarame el valor leido en el puerto serie
+
+int on = 0;                  //variables para controlar el flujo de ejecucion del programa
+int off = 0;
+
+unsigned long sLEDR = 0;     //variable donde almacenaremos el tiempo de millis para controlar el encendido y apagado del led de recepcion sin la necesidad de parar el programa
+int flag = 1;
+
+int R = 0;                   //variables relacionadas con los colores
 int G = 0;
 int B = 0;
 
-void interrupcion1() {
-  if (power == true) {
-    power = false;
-  }
-  if (power == false) {
-    power = true;
+void interrupcion1() {                    //interrupcion donde se controla el estado de prendido y apagado
+  if ((millis() - antirebote1) > 500) {
+    if (power == true) {
+      power = false;
+      cont = 1;
+      flag = 1;
+    }
+    else {
+      power = true;
+      flag = 1;
+    }
+    antirebote1 = millis();
   }
 }
 
 
-void interrupcion2() {
-   if (power >= 1) {
-    cont = cont + 1 ;
+void interrupcion2() {                  //interrupcion donde se controla el estado de cambio de color
+  if ((millis() - antirebote2) > 500) {
+    if (power == true) {
+      cont = 1 ;
+    }
+    antirebote2 = millis();
   }
 }
 
 void setup() {
-  attachInterrupt (digitalPinToInterrupt (BotonEncenderApagar), interrupcion1, FALLING); //Se decalara la interrupción de Encender/Apagar
-  attachInterrupt (digitalPinToInterrupt (BotonCambiarColor), interrupcion2, FALLING);   //Se decalara la interrupción de Cambio de color
+  attachInterrupt (digitalPinToInterrupt (2), interrupcion1, RISING); //Se decalara la interrupción de Encender/Apagar
+  attachInterrupt (digitalPinToInterrupt (3), interrupcion2, RISING);   //Se decalara la interrupción de Cambio de color
 
-  pinMode(LEDREC, OUTPUT);
+  pinMode(LEDRECP, OUTPUT);   //declaramos los pines de los ledes como salidas
   pinMode(LEDCON, OUTPUT);
 
-  //digitalWrite(LEDCON, LOW); // Inicialmente apagado
-
-  Serial.begin(9600);
+  Serial.begin(9600); //iniciamos el puerto serie
 }
 
 void loop() {
-if (Serial.available() > 0) {
- state=Serial.read();
- if (state=='C'){
-  digitalWrite(LEDCON,HIGH);}
-  /*if (power==true){
-    if (cont>=1){
+  if (Serial.available() > 0) { //si hay datos en el puerto serie
+    state = Serial.read();      //leemos el puerto y guardamos el valor leido en la variable state
+
+    if (state == 'C') {         //si se recibe una C es que se debe establecer una conexion
+      on = 1;
+      off = 0;
+    }
+
+    if (state == 'D') {         //si se recibe una D se debe desconectar
+      on = 0;
+      off = 1;
+    }
+
+    if (state == 'R' ) {       //si se recibe una R se nos confirma de que llegaron detos al receptor
+      digitalWrite(LEDRECP, HIGH);
+      sLEDR = millis();
+    }
+  }
+
+  if (on == 1) {               //prendemos el led de conexion establecida
+    digitalWrite(LEDCON, HIGH); 
+  }
+
+  if (power == true and on == 1) { //generamos colores randoms y los enviamos por el puerto serie
+    if (cont >= 1 ) {
       R = random(255);
       G = random(255);
       B = random(255);
-      }
-      cont=0;
-      Serial.print(R);      //envio de colores
-      Serial.println();
-      Serial.print(G);     
-      Serial.println();
-      Serial.print(B);     
-      Serial.println();
 
-      delay(500);                            
+      Serial.print("R");      //envio de colores
+      Serial.print(R);
+      Serial.flush();         //Serial.flush() lo usamos para tener prioridad de envio de datos y esperar a que se termine el envio de los datos anteriores
+      Serial.print("G");
+      Serial.print(G);
+      Serial.flush();
+      Serial.print("B");
+      Serial.print(B);
+      Serial.flush();
+      Serial.print("P");
+
     }
-  else {
-      R = 0;
-      G = 0;
-      B = 0;
-      
-      Serial.print(R);      //envio de colores 0
-      Serial.println();
-      Serial.print(G);     
-      Serial.println();
-      Serial.print(B);     
-      Serial.println();
-
-      delay(500);   
-    }
-
-//añadir confirmacion de recepcio de envio y llegada
+    cont = 0;
   }
-*/
- if (state=='D'){
-  digitalWrite(LEDCON,LOW);
-    }
+
+  if (power == false and flag == 1 and on == 1)  {
+    R = 0;
+    G = 0;                  //enviamos 0 en todos los colores para apagar el led rgb
+    B = 0;
+
+    Serial.print("R");      //envio de colores
+    Serial.print(R);
+    Serial.flush();
+    Serial.print("G");
+    Serial.print(G);
+    Serial.flush();
+    Serial.print("B");
+    Serial.print(B);
+    Serial.flush();
+    Serial.print("P");
+
+    flag = 0;
+  }
+
+  if ((millis() - sLEDR) > tiempoR) { //si el tiempo desde que se prendio el LEDRECP es mayor a 2 segundos se apaga el led
+    digitalWrite(LEDRECP, LOW);
+  }
+
+  if (off == 1) {                   //si se habia recibido un D la variable off se pone en 1 y se apaga el led que indica la conexion
+    digitalWrite(LEDCON, LOW);   
   }
 }
--- PReseptor2/PReseptor2.ino
+++ PReseptor2/PReseptor2.ino
@@ -1,47 +1,76 @@
 #define BotonConectar 2 // Pin del botón de conexión
-const int rgbLedPin = 9; // Pin del LED RGB
 
-volatile bool Connect = false;
-volatile bool flag = false;
+volatile bool Connect = false; //variables de la interrupcion que controla la conexion y desconexion
+volatile bool flag = true;
 
-void interrupcion1() {
-  if (Connect == false) {
-    flag = false;
-    Connect = true;
-  }
-  if (Connect == true) {
-    Connect = false;
-    flag = false;
+int antirebote1 = 0;           //variable para controlar el antirebote del pulsador
+
+#define RPIN 9                 //pines del led rgb segun los colores
+#define GPIN 10
+#define BPIN 11
+
+int Rr = 0;                    //variables donde almacenaremos los valores recibidos para cada color
+int Gr = 0;
+int Br = 0;
+
+int change = 0;                //variable donde almacenaremos el estado de imprimir el color en el led
+
+void interrupcion1() {         //interrupcion donde cambiaremos los estados de unas variables para enviar la solicitud de conexion o desconexion
+  if ((millis() - antirebote1) > 500) {
+    if (Connect == false) {
+      Connect = true;
+      flag = true;
+    }
+    else {
+      Connect = false;
+      flag = true;
+    }
+    antirebote1 = millis();
   }
 }
 
 void setup() {
-  attachInterrupt (digitalPinToInterrupt (BotonConectar), interrupcion1, FALLING);
+  attachInterrupt (digitalPinToInterrupt (BotonConectar), interrupcion1, FALLING);  //definimos la funcion de interrupcion externa de conexion o desxonexion
 
-  pinMode(rgbLedPin, OUTPUT);
-  digitalWrite(rgbLedPin, LOW); // Inicialmente apagado
+  pinMode(RPIN, OUTPUT);   //definimos los pines conectados al led rgb como salidas
+  pinMode(GPIN, OUTPUT);
+  pinMode(BPIN, OUTPUT);
 
-  Serial.begin(9600);
+  Serial.begin(9600);     //inicializamos el puerto serie
 }
 
 void loop() {
-  if (Connect == true) { // Si se presiona el botón de conexión y no está conectado
-    if (flag == false) {
-      Serial.println("connect"); // Solicita conexión al dispositivo 1
-      delay(500); // Debounce
-      flag = true;
-    }
 
-    //añadir recepcion colores
-
-    //añadir confirmacion envio y recepcion
-  }
-  else {
-    if (flag == false) {
-      Serial.println("disconnect"); // Solicita conexión al dispositivo 1
-      delay(500); // Debounce
-      flag = true;
-    }
+  if (Connect == true and flag == true ) { // enviamos un C por el puerto
+    Serial.print('C');
+    flag = false;
   }
 
-}
+  if (Connect == false and flag == true) { // enviamos un D por el puerto
+    Serial.print('D');
+    flag = false;
+  }
+
+  if (Serial.available() > 0) { //si hay datos en el puerto se ejecutan las instrucciones
+    if (Serial.find('R')) {     //Serial.find()se usa para buscar un caracter, palabra definida o palabra de cierto ancho
+      Rr = Serial.parseInt();   //Serial.parseInt() se usa para que cualquier valor numerico que hay este en el puerto se gurade, tiene que se un numero entero
+    }
+    if (Serial.find('G')) {
+      Gr = Serial.parseInt();
+    }
+    if (Serial.find('B')) {
+      Br = Serial.parseInt();
+    }
+
+    if (Serial.find('P')) {
+      change = 1;
+    }
+  }
+if (change == 1) {            //se usa el PWM para poner los valores en el pin del led reb y formar los colores
+      analogWrite(RPIN, Rr);
+      analogWrite(GPIN, Gr);
+      analogWrite(BPIN, Br);
+      change = 0;
+      Serial.print('R');      //envia por el puerto una R para confirmar la recepcion de los datos
+    }
+ }
